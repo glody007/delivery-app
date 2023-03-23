34dfdcb6909917a4221a237d627143179d03f1f3
glody007 <glodymbutwile@gmail.com>
2023-03-23 16:05:00 +0200

    fix: Text color.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text class="text-blue-500">Open up App.js to start working on your app!</Text>
+      <Text className="font-bold text-blue-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
