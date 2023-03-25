b49b1a312958183bd48a01f7770fb8d4fadad519
glody007 <glodymbutwile@gmail.com>
2023-03-25 22:24:12 +0200

    feat: Header and search.

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useLayoutEffect } from 'react';
+import { Image, SafeAreaView, Text, TextInput, View } from 'react-native';
+import { 
+    ChevronDownIcon,
+    UserIcon,
+    AdjustmentsVerticalIcon,
+    MagnifyingGlassIcon
+} from "react-native-heroicons/outline";
 
 export default function Home() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-blue-500">Home screen</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const navigation = useNavigation()
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
+    return (
+        <SafeAreaView className='bg-white'>
+
+            {/* Header */}
+            <View className='flex-row items-center pb-4 space-x-2 px-4'>
+                <Image 
+                    source={{
+                        uri: 'https://links.papareact.com/wru'
+                    }}
+                    className='h-7 w-7 bg-gray-300 rounded-full'
+                />
+                <View className='flex-1'>
+                    <Text className='font-bold text-xs text-gray-300'>Deliver now</Text>
+                    <Text className='font-bold text-xl'>
+                        Current location
+                        <ChevronDownIcon size={20} color='#00CC88' />
+                    </Text>
+                </View>
+
+                <UserIcon size={33} color='#00CC88' />
+            </View>
+
+            {/* Search */}
+            <View className='flex-row items-center pb-4 space-x-2 px-4'>
+                <View className='flex-row flex-1 bg-gray-200 space-x-2 p-3'>
+                    <MagnifyingGlassIcon size={20} color='gray' />
+                    <TextInput placeholder='Restaurant and hotel' />
+                </View>
+
+                <AdjustmentsVerticalIcon size={33} color='#00CC88' />
+            </View>
+        </SafeAreaView>
+    );
 }
