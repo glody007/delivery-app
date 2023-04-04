93f8a16b33472e2eece2530002a5f6c069dc70a4
glody007 <glodymbutwile@gmail.com>
2023-04-04 12:28:26 +0200

    feat: Show basket icon only if there is item.

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -7,16 +7,22 @@ import {
 import { useSelector } from 'react-redux'
 import { selectBasketItems, selectBasketTotal } from '../slices/basketSlice'
 import { formatCurrency } from 'react-native-format-currency'
+import { useNavigation } from '@react-navigation/native'
 
 export default function BasketIcon() {
   const totalBasket = useSelector(selectBasketTotal)
   const items = useSelector(selectBasketItems)
   const [valueFormattedWithoutSymbol, symbol] = formatCurrency({ amount: totalBasket, code: "USD" });
 
+  const navigation = useNavigation()
+
+  if(items.length === 0) return null
+
   return (
     <View className="absolute bottom-10 w-full z-10">
         <TouchableOpacity 
             className="flex-row bg-[#00CCBB] rounded-lg mx-4 p-4"
+            onPress={() => navigation.navigate('Basket')}
         >
             <View className="flex-1 flex-row justify-between items-center">
                 <View className="bg-[#01A196] rounded p-2">
